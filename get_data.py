a551c7bf3ae38896c9441a6c1d142274aa3bb644
Nakarin.Vi <nakarin.v@arcadiaapm.com>
2022-09-20 20:50:14 +0700

    Editing send request function

--- get_data.py
+++ get_data.py
@@ -1,19 +1,58 @@
 from dotmap import DotMap
 import requests
 
+notion_url = 'https://api.notion.com/v1/databases/e98588084035474394b5bec1651c6eef/query'
+
 def set_header():
     header = {
-        'Authorication': 'Bearer Bearer secret_ftVfm6G44cEujeMsnIuxhSGeDV7IMSpZFHiu0YrjLrM',
+        'Authorization': 'Bearer secret_ftVfm6G44cEujeMsnIuxhSGeDV7IMSpZFHiu0YrjLrM',
         'Content Type': 'application/json',
         'Notion-Version': '2022-06-28'
     }
-    return header
+    return DotMap(header)
 
-def set_body():
+def set_body(start_date, end_date, person):
     body = {
-        
+        'filter':{
+            'and': [
+                {
+                    'property': 'Responsible person',
+                    'people': {
+                        'contains': str(person)
+                    }
+                },
+                {
+                    'and': [
+                        {
+                            'property': 'Date',
+                            'date': {
+                                'on_or_before': str(end_date)
+                            }
+                        },
+                        {
+                            'property': 'Date',
+                            'date': {
+                                'on_or_after': str(start_date)
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        'sorts': [
+            {
+                'property': 'Date',
+                'direction': 'ascending'
+            }
+        ]
     }
-    return body
+    return DotMap(body)
 
-def get_data():
+def send_request():
+    x = requests.post(url=str(notion_url), headers=set_header(), json=set_body('2022-09-01', '2022-09-30', 'fb6ab337-1d37-4efc-8730-efbe2c602a9f'))
+    return DotMap(x.json())
+
+print(send_request())
+
+# def get_data():
     
