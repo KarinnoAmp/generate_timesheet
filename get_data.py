aa259740611f4e7e5f86845d2b5bbfcc0f339c67
Nakarin.Vi <nakarin.v@arcadiaapm.com>
2022-09-20 19:54:11 +0700

    Add Library to git

---
+++ get_data.py
@@ -0,0 +1,19 @@
+from dotmap import DotMap
+import requests
+
+def set_header():
+    header = {
+        'Authorication': 'Bearer Bearer secret_ftVfm6G44cEujeMsnIuxhSGeDV7IMSpZFHiu0YrjLrM',
+        'Content Type': 'application/json',
+        'Notion-Version': '2022-06-28'
+    }
+    return header
+
+def set_body():
+    body = {
+        
+    }
+    return body
+
+def get_data():
+    
