5a38d6dee65db1c05fec17454888018c2c4463c1
Rukpong.R <rukpong.r@arcadiaapm.com>
2022-09-27 11:52:10 +0700

    add write output json file

--- get_data.py
+++ get_data.py
@@ -1,7 +1,9 @@
 from dotmap import DotMap
 import requests
+import json
 
 notion_url = 'https://api.notion.com/v1/databases/e98588084035474394b5bec1651c6eef/query'
+
 
 def set_header():
     header = {
@@ -10,6 +12,7 @@ def set_header():
         'Notion-Version': '2022-06-28'
     }
     return DotMap(header)
+
 
 def set_body(start_date, end_date, person):
     body = {
@@ -48,11 +51,21 @@ def set_body(start_date, end_date, person):
     }
     return DotMap(body)
 
+
 def send_request():
     x = requests.post(url=str(notion_url), headers=set_header(), json=set_body('2022-09-01', '2022-09-30', 'fb6ab337-1d37-4efc-8730-efbe2c602a9f'))
     return DotMap(x.json())
 
-print(send_request())
+
+def write_output(data: dict):
+    with open("result.json", "a") as file:
+        json_object = json.dumps(data, indent=4)
+        file.write(json_object)
+    print('Write file output success.')
+
+# print(send_request())
+# write_output(send_request)
+write_output(send_request())
+
 
 # def get_data():
-    
